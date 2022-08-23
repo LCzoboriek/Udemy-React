b45db4b47f59c089a413b717a6cd3d05eea546ca
Luke Czoboriek <lc169782@protonmail.com>
2022-08-23 14:53:04 +0100

    Added react css styling

---
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+//Components are still functions
+import "./ExpenseDate.css";
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear(); // So this way is how you can add javascript syntax within react components and functions, you can
+  //run it inline but its best practice to extract the logic and place it on a seperate line
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,19 +1,14 @@
+import ExpenseDate from "./ExpenseDate";
 //A component in javascript react is a javascript function
 import "./ExpenseItem.css";
 //We now pass in properties as the arguments in the function
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear(); // So this way is how you can add javascript syntax within react components and functions, you can
-  //run it inline but its best practice to extract the logic and place it on a seperate line
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
       {/* You use className to attach the css class to the div, class is a reserved word */}
+      <ExpenseDate date={props.date} />
+      {/* funneling multiple components that are nested inside eachother, were passing data from app into expense item, then outputting some of the data
+      and then forwarding the date stuff even further into the expensedate component, thats the beauty of react */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         {/* The above must match the specified name in the app.js assignment, its done via a key/value pair, this is how 
