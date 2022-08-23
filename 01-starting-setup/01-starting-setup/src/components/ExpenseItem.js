e0a1a02af07eec3907749b6a1d92ef25a7f2d2c5
Luke Czoboriek <lc169782@protonmail.com>
2022-08-23 11:39:35 +0100

    Added in props

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,18 +1,16 @@
 //A component in javascript react is a javascript function
 import "./ExpenseItem.css";
-
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 249.67;
+//We now pass in properties as the arguments in the function
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
       {/* You use className to attach the css class to the div, class is a reserved word */}
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
+        {/* The above must match the specified name in the app.js assignment, its done via a key/value pair  */}
         {/* You use curly braces to import javascript inside html code, they are called dynamic placeholders */}
-        <div className="expense-item__price">{expenseAmount}</div>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
