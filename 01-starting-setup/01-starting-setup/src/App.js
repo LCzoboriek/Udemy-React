bf2de2fdedc7af27aad7314cda4c8305998b87f2
Luke Czoboriek <lc169782@protonmail.com>
2022-08-23 14:19:48 +0100

    Added native javascript in react component

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -6,19 +6,28 @@ function App() {
       id: "e1",
       title: "Toilet Paper",
       amount: 94.12,
+      quantity: 5,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      quantity: 6,
+    },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
+      quantity: 8,
       date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
+      quantity: 1,
       date: new Date(2021, 5, 12),
     },
   ];
@@ -30,21 +39,25 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
+        quantity={expenses[0].quantity}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
+        quantity={expenses[1].quantity}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date} // This process is adding attributes from the above array, and assigning them to received attribute values
+        date={expenses[2].date}
+        quantity={expenses[2].quantity} // This process is adding attributes from the above array, and assigning them to received attribute values
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
-        date={expenses[3].date} // This process is adding attributes from the above array, and assigning them to received attribute values
+        date={expenses[3].date}
+        quantity={expenses[3].quantity} // This process is adding attributes from the above array, and assigning them to received attribute values
       ></ExpenseItem>
     </div> // Html inside a javascript function, this works because of the feature called jsx, it was introduced by the react team
     //This only works due to the transformation stuff behind the scenes
--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -2,15 +2,27 @@
 import "./ExpenseItem.css";
 //We now pass in properties as the arguments in the function
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear(); // So this way is how you can add javascript syntax within react components and functions, you can
+  //run it inline but its best practice to extract the logic and place it on a seperate line
   return (
     <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       {/* You use className to attach the css class to the div, class is a reserved word */}
-      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        {/* The above must match the specified name in the app.js assignment, its done via a key/value pair  */}
+        {/* The above must match the specified name in the app.js assignment, its done via a key/value pair, this is how 
+        you share data between components within react applications, props is a very important concept.  */}
         {/* You use curly braces to import javascript inside html code, they are called dynamic placeholders */}
         <div className="expense-item__price">{props.amount}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>Quantity: {props.quantity}</h2>
       </div>
     </div>
   );
